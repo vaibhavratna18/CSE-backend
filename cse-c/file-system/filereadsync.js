6c4775064c9e5cae05a2e6aa282728c908f87983
vaibhavratna18 <vaibhavratnasingh@gmail.com>
2025-02-11 10:12:07 +0530

    2nd update

--- cse-c/file-system/filereadsync.js
+++ cse-c/file-system/filereadsync.js
@@ -1,3 +1,13 @@
 const fs=require ("fs");
-const data=fs.readFileSync("./data.txt");
-console.log(data)
+const data=fs.readFileSync("./data.txt","utf8");    
+//console.log(data.toString());
+console.log(data);
+if (data.match("H")){
+    console.log("File contains'H'");
+    const newdata = data.replace("H","ABES");
+    fs.writeFileSync("./data.txt",newdata,"utf-8")
+}
+//fs.writeFileSync("data2.txt","Hello Ghaziabad","utf-8");
+//fs.renameSync("./data2.txt","./data3.txt");
+//fs.unlinkSync("./.data1.txt");
+//fs.appendFileSync("./data1.txt","Virat Kohli is the GOAT", "utf-8");
